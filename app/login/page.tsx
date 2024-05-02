cb8ccf7345c09f5afd6900bcba351024fae011f6
Mohamed Oun <mohamed3on@gmail.com>
2024-05-02 13:14:09 +0200

    make login nice

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,18 @@
 import { LoginWithGoogle } from '@/app/login/LoginWithGoogle';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
-export default function Login() {
+export default async function Login() {
+  const supabase = createClient();
+  const { data: userData } = await supabase.auth.getUser();
+
+  if (userData.user) {
+    redirect('/episodes');
+  }
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-8'>
       <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
-        <div className='p-6 space-y-4 md:space-y-6 sm:p-8 text-center'>
+        <div className='p-6 space-y-4 md:space-y-6 sm:p-8 text-center flex flex-col'>
           <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
             Login to Topcasts
           </h1>
