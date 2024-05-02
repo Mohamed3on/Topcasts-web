85d37201079a180191eb6b6591040524ef7d086e
Mohamed Oun <mohamed3on@gmail.com>
2024-05-02 18:03:18 +0200

    debugging

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -5,11 +5,12 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(req: NextRequest) {
   const supabase = createClient();
   const { searchParams, origin } = req.nextUrl;
+  console.log('🚀 ~ GET ~ origin:', origin);
   const code = searchParams.get('code');
 
   if (code) {
     await supabase.auth.exchangeCodeForSession(code);
   }
   // TODO: implement redirect to the previous page
-  return NextResponse.redirect(`${getHost()}/`);
+  return NextResponse.redirect(`${getHost()}/episode/1`);
 }
