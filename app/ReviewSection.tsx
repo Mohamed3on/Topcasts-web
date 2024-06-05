6c0fd13d7f6024de85f087f598a5fde135add985
Mohamed Oun <mohamed3on@gmail.com>
2024-06-05 17:11:55 +0200

    add your review

--- app/ReviewSection.tsx
+++ app/ReviewSection.tsx
@@ -1,9 +1,11 @@
 import { ReviewButtons } from '@/app/ReviewButtons';
 import { ReviewType } from '@/app/api/types';
 import { createClient } from '@/utils/supabase/server';
+import Image from 'next/image';
 
 export const ReviewSection = async ({ episodeId }: { episodeId: number }) => {
   let reviewType: ReviewType | undefined = undefined;
+  let reviewText: string | undefined = undefined;
   const supabase = createClient();
 
   const { data: userData } = await supabase.auth.getUser();
@@ -11,13 +13,33 @@ export const ReviewSection = async ({ episodeId }: { episodeId: number }) => {
   if (userData?.user) {
     const { data } = await supabase
       .from('podcast_episode_review')
-      .select('review_type')
+      .select('review_type, text')
       .eq('episode_id', episodeId)
       .eq('user_id', userData.user.id)
       .single();
 
     reviewType = data?.review_type as ReviewType;
+    reviewText = data?.text as string;
   }
 
-  return <ReviewButtons reviewType={reviewType} episodeId={episodeId} />;
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <ReviewButtons reviewType={reviewType} episodeId={episodeId} />
+
+      {reviewText && (
+        <div className="flex flex-col items-center gap-2 rounded border p-4">
+          <Image
+            unoptimized
+            width={32}
+            height={32}
+            className="rounded-full"
+            src={userData?.user?.user_metadata?.avatar_url}
+            alt="User Image"
+          />
+          <p className=" font-semibold">Your Review</p>
+          <p className="italic">{reviewText}</p>
+        </div>
+      )}
+    </div>
+  );
 };
