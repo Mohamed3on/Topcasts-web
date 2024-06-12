fb188c91d70cdac06832d234d3adf436e9fcb126
Mohamed Oun <mohamed3on@gmail.com>
2024-06-12 15:30:01 +0200

    make description collapsible

--- app/episodes/EpisodeDescription.tsx
+++ app/episodes/EpisodeDescription.tsx
@@ -1,30 +1,48 @@
 'use client';
 
-import DOMPurify from 'dompurify';
-import { useEffect, useState } from 'react';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible';
+
+import DOMPurify from 'isomorphic-dompurify';
+import { ArrowDown } from 'lucide-react';
+import { useState } from 'react';
 
 export const EpisodeDescription = ({
   description,
 }: {
   description: string;
 }) => {
-  const [sanitizedHtml, setSanitizedHtml] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const cleanHtml = DOMPurify.sanitize(description);
 
-  useEffect(() => {
-    const domPurify = DOMPurify(window);
-    const cleanHtml = domPurify.sanitize(description);
-    setSanitizedHtml(cleanHtml);
-  }, [description]);
-
-  if (!sanitizedHtml) return null;
+  if (!cleanHtml) return null;
 
   return (
-    <span
-      className="break-words"
-      dangerouslySetInnerHTML={{
-        __html: sanitizedHtml,
-      }}
-    ></span>
+    <Collapsible open={isOpen} onOpenChange={setIsOpen} className=" space-y-2">
+      <div className="flex justify-center">
+        <CollapsibleTrigger className="flex items-baseline">
+          <h4 className="font-semibold">Episode description</h4>
+          <div
+            className={`ml-2 transform transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+          >
+            <ArrowDown className="flex h-5 w-5" />
+          </div>
+          <span className="sr-only">Toggle description</span>
+        </CollapsibleTrigger>
+      </div>
+
+      <CollapsibleContent className="space-y-2">
+        <span
+          className="break-words"
+          dangerouslySetInnerHTML={{
+            __html: cleanHtml,
+          }}
+        ></span>
+      </CollapsibleContent>
+    </Collapsible>
   );
 };
 
