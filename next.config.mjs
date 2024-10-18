8de7c8aaf94c142226bc0dbeccb99faee6ce04e0
Mohamed Oun <mohamed3on@gmail.com>
2024-10-18 10:35:31 +0200

    disable img optimization cause i'm poor and vercel is expensive

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
