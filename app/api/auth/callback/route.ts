df8da4612983f27c8f1e13bd45df7024fc13bee6
Mohamed Oun <mohamed3on@gmail.com>
2025-10-01 18:41:32 +0200

    added dynamic export to auth callback route

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@/utils/supabase/ssr';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
   const supabase = createClient();
   const { searchParams, origin } = req.nextUrl;
